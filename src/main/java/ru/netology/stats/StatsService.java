10cb9ad809e32cd55e6a0bf070950cff8e4f9c71
Татьяна Смыслова <l.dalecrua@gmail.com>
2022-05-26 13:30:26 +0300

    репозиторий

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,64 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public long sum(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int aboveAvg(long[] sales) {
+        long avg = avg(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int lessThanAverage(long[] sales) {
+        long avg = avg(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long avg(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,73 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMax () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.maxSales(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMin () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageAmount () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.sum(sales) / sales.length;
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void aboveAvg () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.sum(sales) / sales.length-10;
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lessThanAverage () {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        long actual = service.sum(sales) / sales.length-10;
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
